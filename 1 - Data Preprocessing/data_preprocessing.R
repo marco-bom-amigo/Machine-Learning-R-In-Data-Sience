437c93ba5ca5ef5f0da0310862da339d500fbeb7
Bonamichi <marco.bom.amigo@gmail.com>
2018-07-09 13:35:37 -0300

    Taking care of missing data

--- 1 - Data Preprocessing/data_preprocessing.R
+++ 1 - Data Preprocessing/data_preprocessing.R
@@ -3,3 +3,7 @@
 # Importig the dataset
 setwd("1 - Data Preprocessing")
 dataset <- read.csv("data/Data.csv")
+
+# Taking care of missing data
+dataset$Age    <- ifelse(is.na(dataset$Age)   , ave(dataset$Age   , FUN = function(x) mean(x, na.rm = T)), dataset$Age)
+dataset$Salary <- ifelse(is.na(dataset$Salary), ave(dataset$Salary, FUN = function(x) mean(x, na.rm = T)), dataset$Salary)
