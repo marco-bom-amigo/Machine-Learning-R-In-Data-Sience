c0ab80807491920498c1fb8bff8f3161a6e2ee8a
Bonamichi <marco.bom.amigo@gmail.com>
2018-07-09 15:40:53 -0300

    Encoding categorical data

--- 1 - Data Preprocessing/data_preprocessing.R
+++ 1 - Data Preprocessing/data_preprocessing.R
@@ -7,3 +7,7 @@ dataset <- read.csv("data/Data.csv")
 # Taking care of missing data
 dataset$Age    <- ifelse(is.na(dataset$Age)   , ave(dataset$Age   , FUN = function(x) mean(x, na.rm = T)), dataset$Age)
 dataset$Salary <- ifelse(is.na(dataset$Salary), ave(dataset$Salary, FUN = function(x) mean(x, na.rm = T)), dataset$Salary)
+
+# Encoding categorical data
+dataset$Country   <- factor(x = dataset$Country,levels = c("France", "Spain", "Germany"),labels = c(1,2,3))
+dataset$Purchased <- factor(x = dataset$Purchased,levels = c("No", "Yes"),labels = c(0,1))
